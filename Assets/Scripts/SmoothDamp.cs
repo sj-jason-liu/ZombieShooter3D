82d307558cafbfc04687cc4eb67198a3e0f63318
sj-jason-liu <sj.jason.liu@gmail.com>
2021-09-01 23:26:50 +0800

    Added: imported animation from maximo and gun model from Filebase & IK controller to both hands of Player and be able to hold gun.

---
+++ Assets/Scripts/SmoothDamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDamp : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _speed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = _target.transform.position;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * _speed);
+        transform.rotation = Quaternion.Euler(_target.transform.rotation.eulerAngles);
+    }
+}
